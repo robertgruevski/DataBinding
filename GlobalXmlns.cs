c4f58d8cfa3bef28b56c22d3de8fa712fbff7ada
robertgruevski <robertgruevski@outlook.com>
2025-08-27 15:54:47 -0400

    Add project files.

---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "DataBinding")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "DataBinding.Pages")]
